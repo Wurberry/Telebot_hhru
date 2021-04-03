1f93ba5fb0d67f81df1b3fc40a49cd13f2417ae1
Wurberry <36203568+Wurberry@users.noreply.github.com>
2021-04-03 16:33:18 +0300

    Update gettervacancies.py

--- gettervacancies.py
+++ gettervacancies.py
@@ -6,14 +6,14 @@ import sqlite3
 
 #открываем файл со страницами и каждую обрабатываем циклом на вытаскивание вакансий
 for fl in os.listdir('./docs/pagination'):
-    f = open('./docs/pagination/{}'.format(fl), encoding='utf8')
-    jsonText = f.read()
+    f = open('./docs/pagination/{}'.format(fl), encoding='utf8') # получение файла из дериктории
+    jsonText = f.read() # считывание страницы в переменную
     f.close()
 
-    jsonObj = json.loads(jsonText)
+    jsonObj = json.loads(jsonText) # занесение переменной в объект
 
-    # цикл вытаскивающий вакансии из страниц
-    for v in jsonObj['items']:
+    # цикл проходит по каждой вакансии в странице
+    for v in jsonObj['items']: 
         req = requests.get(v['url'])
         data = req.content.decode(encoding='utf8')
         req.close()
